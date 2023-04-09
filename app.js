be84b34720c29077bd44a4c7f451517690a261e2
rmauro808 <rmauro808@gmail.com>
2023-04-09 19:45:39 -0400

    Updating Module Six

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 //allow CORS
 app.use('/api', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow_Headers', 'Origin, X-Requested-With, Content-type, Accept');
-    res.header('Access-Control-Allow_Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
